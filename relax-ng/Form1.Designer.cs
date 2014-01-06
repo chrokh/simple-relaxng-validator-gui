0f7950e3e2cf21a06eaba12657c40aac84f68ff4
chrokh <christopher.okhravi@gmail.com>
2014-01-06 14:09:16 +0100

    Remove todo texts from labels

--- relax-ng/Form1.Designer.cs
+++ relax-ng/Form1.Designer.cs
@@ -67,18 +67,18 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.Size = new System.Drawing.Size(73, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Instance XML (TODO)";
+            this.label1.Text = "Instance XML";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(320, 24);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(158, 13);
+            this.label2.Size = new System.Drawing.Size(118, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "RelaxNG grammar XML (TODO)";
+            this.label2.Text = "RelaxNG grammar XML";
             // 
             // btnValidate
             // 
